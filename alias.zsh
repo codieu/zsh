944f2fedf2edd673c8cbf5c512562fb6de345c7c
codie <codieuwu@gmail.com>
2025-01-13 14:58:45 +0300

    new aliases

--- alias.zsh
+++ alias.zsh
@@ -2,6 +2,16 @@ alias v=nvim
 # alias s=seshie
 alias t=smart_tmux
 
+alias ntpsync='sudo ntpdate -b pool.ntp.org; sudo hwclock -w'
+
+smart_tmux() {
+  if [[ -n $(pgrep tmux) ]]; then
+    tmux a
+  else
+    tmux -f ~/.config/tmux/tmux.conf
+  fi
+}
+
 alias ls="ls --color=auto"
 alias ll="ls -lA"
 alias la="ls -a"
